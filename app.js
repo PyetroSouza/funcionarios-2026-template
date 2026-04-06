d8f05cc59bfbfe862f2a775d53b40adc236125b8
PYETRO FERREIRA DE SOUZA <25205137@cfp127aluno.local>
2026-04-06 14:02:00 -0300

    feat: adicionar atividade

--- app.js
+++ app.js
@@ -2,3 +2,26 @@
 
 import funcionarios from "./funcionarios.json" with { type: "json" }
 
+function criarCard(funcionarios) {
+    const card = document.createElement('div')
+    card.className = 'card'
+
+    const foto = document.createElement('img')
+    foto.src = `./img/${funcionarios.imagem}`
+    foto.alt = `Foto de ${funcionarios.nome}`
+
+    const nome = document.createElement('h3')
+    nome.textContent = funcionarios.nome
+
+    const cargo = document.createElement('span')
+    cargo.textContent = funcionarios.cargo
+
+    card.replaceChildren(foto,nome,cargo)
+
+    return card
+
+}
+
+const card = funcionarios.map(criarCard)
+
+document.getElementById('container').replaceChildren(...card)
